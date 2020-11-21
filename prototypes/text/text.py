c9b947520e090f6328a308231d70432ca43cf595
Steven Fawcett <steve.fawcett@capgemini.com>
2020-11-21 19:39:23 +0000

    Added Simple Text

---
+++ prototypes/text/text.py
@@ -0,0 +1,86 @@
+import pygame
+
+LEFT, CENTER, RIGHT = range(3)
+TOP, MIDDLE, BOTTOM = range(3)
+
+class DigitalCounter(pygame.sprite.Sprite):
+    pos = None
+    pos_rel = None
+
+    def __init__(self, screen, text='text', pos=(0, 0), pos_rel=(LEFT, TOP),
+                 font=None, size=20, color=(255, 255, 255), antialias=True):
+        pygame.sprite.Sprite.__init__(self)
+        self.font = pygame.font.Font(font, size)
+        self.color = color
+        self.text = text
+        self.pos = pos
+        self.pos_rel = pos_rel
+        self.screen = screen
+        self.antialias = antialias
+
+        self.rerender()
+
+    def update(self):
+        pass
+
+    def calculate_position(self):
+        return (
+            self.pos_rel[0]*(self.screen.get_size()[0]/2 - self.rect.width/2)
+            + (1-2*(self.pos_rel[0]/2))*self.pos[0],
+            self.pos_rel[1]*(self.screen.get_size()[1]/2 - self.rect.height/2)
+            + (1-2*(self.pos_rel[1]/2))*self.pos[1],
+            )
+
+    def print_text(self, text, pos=None):
+        self.text = text
+        if pos:
+            self.pos = pos
+        self.rerender()
+
+    def rerender(self):
+        self.image = self.font.render(self.text, self.antialias, self.color)
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = self.calculate_position()
+
+
+if __name__ == '__main__':
+    class Example(object):
+        def __init__(self):
+            pass
+
+        def initialize(self):
+            pygame.init()
+            self.screen = pygame.display.set_mode((640, 480))
+            pygame.display.set_caption('textrender example')
+
+            self.clock = pygame.time.Clock()
+            self.sprites = pygame.sprite.RenderUpdates()
+
+            self.counter = DigitalCounter(
+                self.screen, 'FPS', size=42, pos=[50, 50],
+                pos_rel=(RIGHT, BOTTOM))
+
+            self.sprites.add(self.counter)
+
+            self.background = pygame.Surface(self.screen.get_size()).convert()
+            self.background.fill((25, 25, 125))
+
+            self.screen.blit(self.background, (0, 0))
+            pygame.display.update()
+
+        def run(self):
+            self.initialize()
+
+            counter = 0 
+            while True:
+                counter += 1
+                self.clock.tick(30)
+                self.counter.print_text('%d' % counter )
+                self.sprites.clear(self.screen, self.background)
+
+                dirty = self.sprites.draw(self.screen)
+                pygame.display.update(dirty)
+
+
+    Example().run()
+
