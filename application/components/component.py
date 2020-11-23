14575f92f0cc61c68421bfe5e841cf1f4b7279c9
Steven Fawcett <steve.fawcett@capgemini.com>
2020-11-23 00:07:51 +0000

    added inheritance class

---
+++ application/components/component.py
@@ -0,0 +1,40 @@
+import pygame
+import os
+
+
+class Component(pygame.sprite.Sprite):
+
+    RESOURCES = os.path.join( os.path.dirname( __file__ ) , 'resources' )
+
+    def __init__(self, image_file="" , angle=360 ,  pos=(0, 0) , size=(500, 500)):
+        super(Component, self).__init__()
+        self.image_file = pygame.image.load( os.path.join( self.RESOURCES, image_file)) 
+        self.original_image = None 
+        self.pos = pos
+        self._angle  = angle 
+
+    def update(self , scale=.3 ):
+        if self.original_image is None :
+            self.original_image = self.image_file.convert()
+            self.original_image = pygame.transform.rotozoom(self.image_file, 0, scale )
+            self.image = self.original_image
+            self.rect = self.original_image.get_rect()
+            self.rect.center = self.pos
+
+
+    @property
+    def angle(self):
+        return self._angle
+
+    @angle.setter
+    def angle(self, value):
+        self._angle = value
+
+        if self._angle < 0 : self._angle = 360
+        if self._angle > 360 : self._angle = 1
+
+        if self.original_image is not None:
+            self.image = pygame.transform.rotate(self.original_image, self._angle)
+            x, y = self.rect.center  
+            self.rect = self.image.get_rect()  
+            self.rect.center = (x, y)  
--- application/components/pointer.py
+++ application/components/pointer.py
@@ -1,28 +1,12 @@
 import pygame
 import os
+import application.components.component as component
 
-RESOURCES = os.path.join( os.path.dirname( __file__ ) , 'resources' )
-
-class Pointer(pygame.sprite.Sprite):
+class Pointer( component.Component ):
 
     def __init__(self, pos=(0, 0) , size=(500, 500)):
-        super(Pointer, self).__init__()
-        self.image_file = pygame.image.load( os.path.join( RESOURCES, 'pointer.png')) 
-        self.original_image = None 
-        self.angle = 0
-        self.pos = pos
+        super(Pointer, self).__init__( pos=pos , size=size , image_file='pointer.png' )
 
     def update(self):
-        if self.original_image is None :
-            self.original_image = self.image_file.convert()
-            self.original_image = pygame.transform.rotozoom(self.image_file, 0, .1)
-            self.rect = self.original_image.get_rect()
-            self.rect.center = self.pos
-
-        self.image = pygame.transform.rotate(self.original_image, self.angle)
-        self.angle -= 1 
-        if self.angle < 0 :
-            self.angle = 360
-        x, y = self.rect.center  # Save its current center.
-        self.rect = self.image.get_rect()  # Replace old rect with new rect.
-        self.rect.center = (x, y)  # Put the new rect's center at old center.
+        self.angle -= 1
+        super(Pointer, self).update( scale=.1 )
